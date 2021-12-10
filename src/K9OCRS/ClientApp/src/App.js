c21c48c9f63fd55b94cd0cd8a38089f4be4cea80
David Rojas <deznorth@gmail.com>
2021-12-10 10:00:14 -0500

    SBIGNIT-30 - Remove the CRA example pages

--- src/K9OCRS/ClientApp/src/App.js
+++ src/K9OCRS/ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import ClassTypesDashboard from './pages/ClassTypesDashboard';
 import ClassTypeSetup from './pages/ClassTypeSetup';
 import CreatePassword from './pages/Account/Create';
@@ -23,8 +21,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route path='/' component={Home} exact />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/classtypes' component={ClassTypesDashboard} />
         <Route path='/classtypes/:classTypeId' component={ClassTypeSetup} />
         <Route path='/Account/Create' component={CreatePassword} />
