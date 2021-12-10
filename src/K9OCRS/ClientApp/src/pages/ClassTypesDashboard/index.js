b4e2d622f356a7eca1522d7bba918abd8a6a4f22
David Rojas <deznorth@gmail.com>
2021-12-10 10:10:35 -0500

    SBIGNIT-30 - Proxy to the api

--- src/K9OCRS/ClientApp/src/pages/ClassTypesDashboard/index.js
+++ src/K9OCRS/ClientApp/src/pages/ClassTypesDashboard/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Container } from 'reactstrap';
 import ClassTypeGrid from './ClassTypeGrid';
 
 const ClassTypesDashboard = () => {
+  const [test, setTest] = useState([]);
+  useEffect(() => {
+    async function getTest() {
+      try {
+        const res = await axios.get('/api/classtypes');
+        console.log(res);
+        setTest(res?.data);
+      } catch(err) {
+        console.log(err?.response);
+        setTest(err?.response?.data);
+      }
+    }
+    getTest();
+  }, []);
+
   return (
     <Container>
       <h1>Class Types Management</h1>
+      <pre>{JSON.stringify(test, null, 2)}</pre>
       <ClassTypeGrid />
     </Container>
   );
