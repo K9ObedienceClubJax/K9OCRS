b4094814af1ac23891fd4837acf43291b8bcd231
David Rojas <deznorth@gmail.com>
2021-12-10 19:03:44 -0500

    SBIGNIT-31 - Create entities and add to DbTables

---
+++ src/DataAccess/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DataAccess.Entities
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public int UserRoleID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ProfilePictureFilename { get; set; } // add a default later
+    }
+}
