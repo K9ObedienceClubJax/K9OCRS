43dfc1525fd31b340e7eed0bc5c0ad1fa7328695
Fernando <jimenezf629@gmail.com>
2021-12-14 13:05:30 -0500

    PBIGNIT-15 Added dog controller

---
+++ src/K9OCRS/Controllers/DogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DataAccess.Entities; //change models to entities
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace K9OCRS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogController : Controller
+    {
+        [HttpPost]
+
+    }
+}
