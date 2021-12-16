4d8a4222af2b94f9976a78b07fbcf97d7f39fd80
David Rojas <deznorth@gmail.com>
2021-12-15 19:47:02 -0500

    SBIGNIT-35 - Add SQL script to clean DB tables

---
+++ SQL/K9_CleanTables.sql
@@ -0,0 +1,28 @@
+
+DELETE FROM ClassMeetings;
+DBCC CHECKIDENT ('[ClassMeetings]', RESEED, 0);
+
+DELETE FROM VaccinationRecords;
+
+DELETE FROM SectionApplications;
+DBCC CHECKIDENT ('[SectionApplications]', RESEED, 0);
+
+DELETE FROM UserDogs;
+
+DELETE FROM ClassPhotos;
+DBCC CHECKIDENT ('[ClassPhotos]', RESEED, 0);
+
+DELETE FROM ClassSections;
+DBCC CHECKIDENT ('[ClassSections]', RESEED, 0);
+
+DELETE FROM ClassTypes;
+DBCC CHECKIDENT ('[ClassTypes]', RESEED, 0);
+
+DELETE FROM Users;
+DBCC CHECKIDENT ('[Users]', RESEED, 0);
+
+DELETE FROM UserRoles;
+DBCC CHECKIDENT ('[UserRoles]', RESEED, 0);
+
+DELETE FROM Dogs;
+DBCC CHECKIDENT ('[Dogs]', RESEED, 0);
