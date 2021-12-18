a550e8a02031baebea7dd018ed4f4ebf68790631
David Rojas <deznorth@gmail.com>
2021-12-18 14:28:36 -0500

    SBIGNIT-35 - PageHeader component improvements

--- src/K9OCRS/ClientApp/src/sharedComponents/PageHeader/index.js
+++ src/K9OCRS/ClientApp/src/sharedComponents/PageHeader/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Col, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Col, Breadcrumb, BreadcrumbItem, UncontrolledAlert } from 'reactstrap';
 
 import './styles.scss';
 
@@ -8,42 +8,54 @@ const PageHeader = props => {
   const {
     title,
     breadCrumbItems,
+    alerts,
     children
   } = props;
 
   const cn = 'pageHeader';
 
   return (
-    <div className={`${cn} d-flex flex-column flex-xl-row  justify-content-xl-between mb-4`}>
-      <Col className={`${cn}__left mb-4`}>
-        <h1>{title}</h1>
-        { breadCrumbItems?.length > 0 &&
-          <Breadcrumb className={`${cn}__breadcrumbs`}>
-            {breadCrumbItems?.map(item => (
-              <BreadcrumbItem
-                key={item.label}
-                tag="a"
-                href={item.path}
-                active={item.active}
-              >
-                {item.label}
-              </BreadcrumbItem>
-            ))}
-          </Breadcrumb>
-        }
-      </Col>
-      <Col className={`${cn}__right mb-4`}>
-        <div className='d-grid gap-2 d-md-flex justify-content-md-end'>
-          {children}
-        </div>
-      </Col>
-    </div>
+    <>
+      <div className={`${cn} d-flex flex-column flex-xl-row  justify-content-xl-between`}>
+        <Col className={`${cn}__left mb-4`}>
+          <h1>{title}</h1>
+          { breadCrumbItems?.length > 0 &&
+            <Breadcrumb className={`${cn}__breadcrumbs`}>
+              {breadCrumbItems?.map(item => (
+                <BreadcrumbItem
+                  key={item.label}
+                  tag="a"
+                  href={item.path}
+                  active={item.active}
+                >
+                  {item.label}
+                </BreadcrumbItem>
+              ))}
+            </Breadcrumb>
+          }
+        </Col>
+        <Col className={`${cn}__right mb-4`}>
+          <div className='d-grid gap-2 d-md-flex justify-content-md-end'>
+            {children}
+          </div>
+        </Col>
+      </div>
+      <div className={`${cn}__alerts mb-4`}>
+
+        { Array.isArray(alerts) && alerts.map((a, idx) => (
+          <UncontrolledAlert key={`${a}_${idx}`}  color={a.color}>
+            {a.message}
+          </UncontrolledAlert>
+        ))}
+      </div>
+    </>
   );
 };
 
 PageHeader.defaultProps = {
   breadCrumbItems: [],
   children: [],
+  alerts: [],
 };
 
 PageHeader.propTypes = {
@@ -53,6 +65,12 @@ PageHeader.propTypes = {
       label: PropTypes.string,
       path: PropTypes.string,
       active: PropTypes.bool,
+    }),
+  ),
+  alerts: PropTypes.arrayOf(
+    PropTypes.shape({
+      color: PropTypes.string,
+      message: PropTypes.string,
     }),
   ),
   children: PropTypes.oneOfType([
