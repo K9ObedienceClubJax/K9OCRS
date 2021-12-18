f5653a626c5d1bc0189fdc843bc3387e2f284841
David Rojas <deznorth@gmail.com>
2021-12-18 14:27:09 -0500

    SBIGNIT-35 - Color changes and Comments

--- src/K9OCRS/ClientApp/src/util/formData.js
+++ src/K9OCRS/ClientApp/src/util/formData.js
@@ -1,4 +1,10 @@
 
+/**
+ * Takes the files array you get from an html file input and makes it
+ * into FormData so it can be sent to the API
+ * @param {FormFileCollection} files
+ * @returns FormData
+ */
 export const filesToFormData = files => {
   var formData = new FormData();
 
