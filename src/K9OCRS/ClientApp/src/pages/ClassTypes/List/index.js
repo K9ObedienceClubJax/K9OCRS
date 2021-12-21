4ee5a66eb69302758d48575c9cbe1e25522a4551
David Rojas <deznorth@gmail.com>
2021-12-20 19:08:41 -0500

    SBIGNIT-35 - Class Types List

--- src/K9OCRS/ClientApp/src/pages/ClassTypes/List/index.js
+++ src/K9OCRS/ClientApp/src/pages/ClassTypes/List/index.js
@@ -4,11 +4,22 @@ import { Button, Container, Row, Spinner } from 'reactstrap';
 import * as classTypesClient from '../../../util/apiClients/classTypes';
 import PageHeader from '../../../sharedComponents/PageHeader';
 import ClassTypeCard from './ClassTypeCard';
+import CustomPagination from '../../../sharedComponents/Pagination';
 
 const ClassTypesList = () => {
+  const itemsPerPage = 8;
+
   const [loading, setLoading] = useState(true);
   const [alerts, setAlerts] = useState([]);
   const [classTypes, setClassTypes] = useState([]);
+  const [page, setPage] = useState(1);
+
+  // Calculate the start and end of our paginated subset of class types
+  const pageStartIndex = (page - 1) * itemsPerPage;
+  const pageEndIndex = page * itemsPerPage;
+
+  // Slice the data up to the max amount of items per page
+  const visibleClassTypes = classTypes?.slice(pageStartIndex, pageEndIndex);
 
   useEffect(() => {
     async function getTest() {
@@ -40,13 +51,21 @@ const ClassTypesList = () => {
         { loading ? <Spinner /> : (
           <Row className="my-4">
             {
-              classTypes?.length > 0 ? classTypes.map(ct => (
+              visibleClassTypes?.length > 0 ? visibleClassTypes.map(ct => (
                 <ClassTypeCard key={ct.id} {...ct} />
               )) : <p>There's no Class Types to show at the moment. Lets <Link to="/Manage/ClassTypes/Add">add the first one</Link>!</p>
             }
           </Row>
         )}
       </Container>
+      <div className="d-flex justify-content-end px-4 px-lg-5">
+        <CustomPagination
+          onPageChange={setPage}
+          totalCount={classTypes?.length}
+          currentPage={page}
+          pageSize={itemsPerPage}
+        />
+      </div>
     </div>
   );
 };
