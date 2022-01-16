2740c1ed7ad46164bfd351e664f72a73a7bc5dc9
David Rojas <deznorth@gmail.com>
2022-01-16 13:31:56 -0500

    SBIGNIT-39 - Include sections with class types

--- src/K9OCRS/Models/ClassManagement/ClassSectionResult.cs
+++ src/K9OCRS/Models/ClassManagement/ClassSectionResult.cs
@@ -7,18 +7,23 @@ namespace K9OCRS.Models.ClassManagement
 {
     public class ClassSectionResult : ClassSection
     {
+        public IEnumerable<ClassMeeting> Meetings { get; set; }
+
+        /// <example>2021-12-26T13:00:00</example>
+        public DateTime StartDate { get; set; }
+
+        /// <example>2022-02-06T15:00:00</example>
+        public DateTime EndDate { get; set; }
+        // Change the data type to a DTO when available
+        public User Instructor { get; set; }
+
+        public ClassSectionResult(ClassSection entity) : base(entity) { }
         public ClassSectionResult(ClassSection entity, IEnumerable<ClassMeeting> meetings) : base(entity)
         {
             Meetings = meetings;
             StartDate = Meetings.Min().StartDate;
             EndDate = Meetings.Max().EndDate;
         }
-        public ClassSectionResult(ClassSection entity, IEnumerable<ClassMeeting> meetings, ClassTypeResult classType) : this(entity, meetings) => ClassType = classType;
-
-        public IEnumerable<ClassMeeting> Meetings { get; set; }
-        /// <example>2021-12-26T13:00:00</example>
-        public DateTime StartDate { get; set; }
-        /// <example>2022-02-06T15:00:00</example>
-        public DateTime EndDate { get; set; }
+        public ClassSectionResult(ClassSection entity, IEnumerable<ClassMeeting> meetings, User instructor) : this(entity, meetings) => Instructor = instructor;
     }
 }
