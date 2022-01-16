1d39a7d900c08827d8f5853ab2ea033fbfb62a92
David Rojas <deznorth@gmail.com>
2022-01-16 13:29:11 -0500

    SBIGNIT-39 - Add more ways to get by id(s)

--- src/DataAccess/Repositories/Contracts/IRepository.cs
+++ src/DataAccess/Repositories/Contracts/IRepository.cs
@@ -7,6 +7,9 @@ namespace DataAccess.Repositories.Contracts
     public interface IRepository<T> where T : class
     {
         Task<T> GetByID(IDbConnection conn, int id);
+        Task<IReadOnlyList<T>> GetByID(IDbConnection conn, string idColumn, int id);
+        Task<IReadOnlyList<T>> GetByIDs(IDbConnection conn, IEnumerable<int> ids);
+        Task<IReadOnlyList<T>> GetByIDs(IDbConnection conn, string idColumn, IEnumerable<int> ids);
         Task<IReadOnlyList<T>> GetAll(IDbConnection conn);
         Task<int> Add(IDbConnection conn, T entity);
         Task<int> Add(IDbConnection conn, IDbTransaction tr, T entity);
