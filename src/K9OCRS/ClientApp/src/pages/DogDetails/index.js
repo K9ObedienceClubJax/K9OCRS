a4cf2a154758a6e6e4a9ffc6e6718db3b24159f2
Fernando <jimenezf629@gmail.com>
2022-01-22 23:17:05 -0500

    Updated Dog Details layout

--- src/K9OCRS/ClientApp/src/pages/DogDetails/index.js
+++ src/K9OCRS/ClientApp/src/pages/DogDetails/index.js
@@ -17,27 +17,29 @@ const dogDetails = ({
     })  => {
     return (
         <Container fluid>
-            <div className='d-flex flex-xs-column flex-sm-row'>
-                <Col className='py-1' xs='3'>
-                    <Row><h2 className='dogName'>{dogName}</h2></Row>
-                    <Row><Picture /></Row>
-                </Col>   
-                <Col className='py-5' xs='3'>
-                    <Row><h3>Breed</h3></Row>
-                    <Row><p className='labelValue'>{breed}</p></Row>
-                    <Row><h3>Age</h3></Row>
-                    <Row><p className='labelValue'>{age}</p></Row>
-                    <Row><h3>Current Class</h3></Row>
-                    <Row><p className='labelValue'>{currentClass}</p></Row>
-                </Col>
-                <Col className='py-5' xs='3'>
-                    <Row><h3>Date of Birth</h3></Row>
-                    <Row><p className='labelValue'>{dateOfBirth}</p></Row>
-                    <Row><h3>Date of Vaccination</h3></Row>
-                    <Row><p className='labelValue'>{vaccineDate} ({vaccineApproval})</p></Row>
-                    <Row><h3>Classes Taken</h3></Row>
-                    <Row><p className='labelValue'>{classesTaken.length}</p></Row>
-                </Col>
+            <PageHeader title='My Dogs'/>
+            <div className='row justify-content-lg-center justify-content-md-center justify-content-sm-center justify-content-xs-center'> 
+                <div className='col-xl-2 col-lg-3 col-md-3 row-sm-12 row-xs-12'>
+                    <h2 className='dogName'>{dogName}</h2>
+                    <Picture />
+                    <br/>
+                </div>
+                <div className='col-xl-2 col-lg-3 col-md-3 row-sm-12 row-xs-12'>
+                    <h3>Breed:</h3>
+                    <div className='labelValue'>{breed}</div><br/>
+                    <h3>Age:</h3>
+                    <div className='labelValue'>{age}</div><br/>
+                    <h3>Current Class:</h3>
+                    <div className='labelValue'>{currentClass}</div><br/>
+                </div>
+                <div className='col-xl-2 col-lg-3 col-md-3 row-sm-12 row-xs-12'>
+                    <h3>Date of Birth:</h3>
+                    <div className='labelValue'>{dateOfBirth}</div><br/>
+                    <h3>Date of Vaccination:</h3>
+                    <div className='labelValue'>{vaccineDate} ({vaccineApproval})</div><br/>
+                    <h3>Classes taken:</h3>
+                    <div className='labelValue'>{classesTaken.length}</div>
+                </div>
             </div>
         </Container>
     );
--- src/K9OCRS/Controllers/DogController.cs
+++ src/K9OCRS/Controllers/DogController.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using DataAccess.Entities; //change models to entities
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace K9OCRS.Controllers
 {
-    //[Route("api/[controller]")]
-    //[ApiController]
-    //public class DogController : Controller
-    //{
-    //    [HttpPost]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogController : ControllerBase
+    {
+        //access cofiguration from appsettings.json
+        private readonly IConfiguration _configuration;
 
-    //}
+        public DogController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //get dog details
+        //[HttpGet]
+        //public JsonResult Get()
+        //{
+            
+        //}
+    }
 }
