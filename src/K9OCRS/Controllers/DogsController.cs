bad09194a6bc8bbd2729c89d53d004a4009f2d22
Fernando <jimenezf629@gmail.com>
2022-01-30 02:11:42 -0500

    Create CRUD functions for dog controller

--- src/K9OCRS/Controllers/DogsController.cs
+++ src/K9OCRS/Controllers/DogsController.cs
@@ -47,6 +47,26 @@ namespace K9OCRS.Controllers
             this.serviceConstants = serviceConstants;
         }
 
+        //create dog
+        [HttpPost]
+        public async Task<IActionResult> CreateDog(Dog entity)
+        {
+            try
+            {
+                var result = await connectionOwner.Use(conn =>
+                {
+                    return dbOwner.Dogs.Add(conn, entity);
+                });
+
+                return Ok(result);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        //get list of dogs
         [HttpGet]
         public async Task<IActionResult> GetDogs()
         {
@@ -66,6 +86,64 @@ namespace K9OCRS.Controllers
             }
         }
 
-        
+        //get a dog by Id
+        [HttpGet]
+        public async Task<IActionResult> GetDog(int Id)
+        {
+            try
+            {
+                var entities = await connectionOwner.Use(conn =>
+                {
+                    return dbOwner.Dogs.GetByID(conn, Id);
+                });
+
+                //returns list of dogs
+                return Ok(entities);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        //update dog
+        [HttpPost]
+        public async Task<IActionResult> UpdateDog(Dog entity)
+        {
+            try
+            {
+                var result = await connectionOwner.Use(conn =>
+                {
+                    return dbOwner.Dogs.Update(conn, entity);
+                });
+
+                return Ok(result);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        //delete dog
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDog(int id)
+        {
+            try
+            {
+                var result = await connectionOwner.Use(conn =>
+                {
+                    return dbOwner.Dogs.Delete(conn, id);
+                });
+
+                return Ok(result);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+
     }
 }
