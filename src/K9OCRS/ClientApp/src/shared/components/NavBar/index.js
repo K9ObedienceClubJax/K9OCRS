4857a4b5631c4ce5c58445fc5571c8945518b1ad
David Rojas <deznorth@gmail.com>
2022-01-31 03:17:38 -0500

    SBIGNIT-67 - Implemented Access Based NavBar

--- src/K9OCRS/ClientApp/src/shared/components/NavBar/index.js
+++ src/K9OCRS/ClientApp/src/shared/components/NavBar/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
 import {
   Collapse,
   Navbar,
@@ -10,90 +11,98 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Nav } from "reactstrap";
+import { isLoggedIn, isAtLeastInstructor, isAdmin } from '../../../util/accessEvaluator';
+import selectors from "../../modules/selectors";
+import { logout as logoutAction } from '../../../areas/accounts/modules/actions';
 
 import './style.scss';
 
-export default class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = props => {
+  const { currentUser, logout } = props;
+  const [collapsed, setCollapsed] = useState(true);
 
-  constructor(props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true,
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  }
-
-  render() {
-    return (
-        <Navbar
-          className="k9-navbar navbar-toggleable-xl flex-xl-row-reverse"
-          color="primary"
-          expand="xl"
-          light
+  return (
+      <Navbar
+        className="k9-navbar navbar-toggleable-xl flex-xl-row-reverse"
+        color="primary"
+        expand="xl"
+        light
+      >
+        <NavbarBrand className="m-0"/>
+        <NavbarToggler onClick={() => setCollapsed(!collapsed)} />
+        <Collapse
+          className="d-xl-flex justify-content-end"
+          isOpen={!collapsed}
+          navbar
         >
-          <NavbarBrand className="m-0"/>
-          <NavbarToggler onClick={this.toggleNavbar} />
-          <Collapse
-            className="d-xl-flex justify-content-end"
-            isOpen={!this.state.collapsed}
-            navbar
-          >
-            <Nav className="flex-column flex-xl-row">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">
-                  Browse Classes
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/Account/Classes">
-                  My Classes
-                </NavLink>
-              </NavItem>
+          <Nav className="flex-column flex-xl-row">
+            <NavItem>
+              <NavLink tag={Link} className="text-dark" to="/">
+                Browse Classes
+              </NavLink>
+            </NavItem>
+            { isLoggedIn(currentUser) && (
+              <>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/Account/Classes">
+                    My Classes
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/Account/MyDogs">
+                    My Dogs
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/Account/Membership">
+                    My Membership
+                  </NavLink>
+                </NavItem>
+                {/* This my account link is temporary */}
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/Account">
+                    My Account
+                  </NavLink>
+                </NavItem>
+              </>
+            )}
+            { isAtLeastInstructor(currentUser) && (
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/Account/Sections">
                   My Sections
                 </NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/Account/MyDogs">
-                  My Dogs
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/Account/Membership">
-                  My Membership
-                </NavLink>
-              </NavItem>
+            )}
+            { isAdmin(currentUser) && (
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/Manage">
                   Management
                 </NavLink>
               </NavItem>
-              {/* This my account link is temporary */}
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/Account">
-                  My Account
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <div className="ms-5 my-3 my-xl-0">
-              <Link to="/Account/Login" className="pe-2">
-                <Button color="light" outline>Login</Button>
-              </Link>
-              <Link to="/Account/Create">
-                <Button color="secondary">Get Started!</Button>
-              </Link>
-            </div>
-          </Collapse>
-        </Navbar>
-    );
-  }
+            )}
+          </Nav>
+          <div className="ms-5 my-3 my-xl-0">
+            { !isLoggedIn(currentUser) && (
+              <>
+                <Link to="/Account/Login" className="pe-2">
+                  <Button color="light" outline>Login</Button>
+                </Link>
+                <Link to="/Account/Create">
+                  <Button color="secondary">Get Started!</Button>
+                </Link>
+              </>
+            )}
+            { isLoggedIn(currentUser) && (
+              <Button onClick={() => logout()} color="secondary" outline>Logout</Button>
+            )}
+          </div>
+        </Collapse>
+      </Navbar>
+  );
 }
+
+export default connect(state => ({
+  currentUser: selectors.selectCurrentUser(state),
+}), {
+  logout: logoutAction,
+})(NavMenu);
