a9d468bddae6b8247beb268cbbd14453c44cb55f
Jonathan <jonathan.d.depaul@gmail.com>
2022-02-20 15:35:28 -0500

    Connected class detail page to API

--- src/K9OCRS/ClientApp/src/areas/applications/Catalog/Classes.js
+++ src/K9OCRS/ClientApp/src/areas/applications/Catalog/Classes.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import * as classTypesClient from '../../../util/apiClients/classTypes';
+import { CardImg } from "reactstrap";
 
 const Classes = props => {
     const location = useLocation();
-    const title = location.state.title;
-    const description = location.state.description;
-    const requirements = location.state.requirements;
+    const id = location.state.id;
+    const [classDetail, setClassDetail] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [alerts, setAlerts] = useState([]);
+    // const description = location.state.description;
+    // const requirements = location.state.requirements;
+
+    useEffect(() => {
+        async function getTest() {
+          try {
+            const res = await classTypesClient.getClassTypeByID(id);
+            setClassDetail(res?.data);
+            setLoading(false);
+          } catch(err) {
+            setLoading(false);
+            setAlerts([{ color: 'danger', message: 'We\'re having issues getting the details for this class' }]);
+          }
+        }
+        getTest();
+      }, [id]);
 
     return (
         <>
-        <h2>{title} </h2>
-        <p>Class Catalog / {title}</p>
+        <h2>{classDetail.title} </h2>
+        
+        <p>Class Catalog / {classDetail.title}</p>
         <div className="container">
+        <CardImg
+          alt={`Image for the ${classDetail.title} class`}
+          src={classDetail.imageUrl}
+          top
+        />
         <h5>Description</h5>
-        <p>{description}</p>
+        <p>{classDetail.description}</p>
         <h5>Requirements</h5>
-        {(typeof {requirements} !== 'undefined') ? <p>{requirements}</p> : <p>This class has no requirements</p>}
+        {classDetail.requirements ? <p>{classDetail.requirements}</p> : <p>This class has no requirements</p>}
+        
         </div>
         </>
     )
