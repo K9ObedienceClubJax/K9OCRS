3c03a9adfd368dbbcb4cfbb0f5dabb31a2c1d529
Jonathan <jonathan.d.depaul@gmail.com>
2022-03-30 00:07:35 -0400

    Redesign of confirmation page - incomplete, for demonstration

--- src/K9OCRS/ClientApp/src/areas/applications/Confirmation/index.js
+++ src/K9OCRS/ClientApp/src/areas/applications/Confirmation/index.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import selectors from "../../../shared/modules/selectors";
+import PageHeader from '../../../shared/components/PageHeader';
+import { Alert, Button } from 'reactstrap';
+import Avatar from '../../../shared/components/Avatar';
 
 
 const Confirm = props => {
@@ -12,7 +15,7 @@ const Confirm = props => {
   // TODO: remove the eslint comments from the lines below and actually use the loading and alerts variables.
   const [loading, setLoading] = useState(true); // eslint-disable-line
   const [alerts, setAlerts] = useState([]); // eslint-disable-line
-  const { currentUser } = props;
+  //const { currentUser } = props;
 
   useEffect(() => {
       async function getTest() {
@@ -30,63 +33,39 @@ const Confirm = props => {
       getTest();
     }, [sectionId]);
 
-    const availableSeats = sectionDetail?.rosterCapacity - sectionDetail?.rosterActual;
     
   return (
     <div className="container">
-      <h3>Thank you for choosing the following class:</h3>
-      <div className="mt-3 mb-3">
-        <div className="card">
-          <div className="card-header">
-            <h3>{sectionDetail?.classType?.title}</h3>
-          </div>
-          <div className="card-body">
-          {sectionDetail?.classType?.description}
-          </div>
-          <div className="card-footer">
-            <span className="text-center font-weight-bold">
-              Section: {sectionDetail?.id} | Day: Monday | Time: 6PM | Start: {sectionDetail?.startDate} |
-              Duration: 7 weeks | Instructor: {sectionDetail?.instructor?.firstName} {sectionDetail?.instructor?.lastName} | Available Seats: {availableSeats}
-            </span>
-          </div>
-        </div>
-      </div>
-      <h4>Please confirm your information:</h4>
-      <ul>
-        <li>
-          <span className="font-weight-bold">Student Name: </span>{currentUser.firstName} {currentUser.lastName}
-        </li>
-        <li>
-          <span className="font-weight-bold">Email:</span> {currentUser.email}
-        </li>
-      </ul>
-      <p>Or update below:</p>
-      <form className="mt-3">
-        <label className="form-control-label">
-          <span className="font-weight-bold">First Name: </span></label>
-        <input
-          className="form-control"
-          type="text"
-          id="firstName"
-          placeholder={currentUser.firstName}
-        />
-        <label className="form-control-label">
-          <span className="font-weight-bold">Last Name: </span></label>
-        <input
-          className="form-control"
-          type="text"
-          id="lastName"
-          placeholder={currentUser.lastName}
-        />
-        <label className="form-control-label">
-          <span className="font-weight-bold">Email:</span></label>
-        <input
-          className="form-control"
-          type="email"
-          id="email"
-          placeholder={currentUser.email}
-        />
-      </form>
+      <PageHeader
+          title={sectionDetail?.classType?.title ?? 'Confirm'}
+          alerts={alerts}
+          breadCrumbItems={[
+            { label: 'Class Catalog', path: '/' },
+            { label: sectionDetail?.classType?.title ?? 'Confirm', active: true },
+          ]}
+          setAlerts={setAlerts}
+        >
+      <Button color='light'>
+          Cancel
+        </Button>
+      <Button color='primary'>
+          Submit Application
+        </Button>
+        </PageHeader>
+        <Alert color="primary"><h3>Review your application</h3>
+          <span>Please review the class' details, then select the dog that you want to register for this class section and verify that the dog's information is comlete and up to date.</span>
+        </Alert>
+        <h4>Class Instructor</h4>
+        <p className="align-middle"><Avatar imageUrl={sectionDetail?.instructor?.profilePictureUrl} /> {sectionDetail?.instructor?.firstName} {sectionDetail?.instructor?.lastName}</p>
+        <h4>Class Schedule</h4>
+        <i>Note that these dates and times are subject to change</i>
+        <ol>
+          {sectionDetail?.meetings?.map((meeting) => (
+            <li key={meeting.id}>{meeting.startDate}</li>
+          ))}
+        </ol>
+        <h4>Class Requirements</h4>
+        <p>{sectionDetail?.classType?.requirements}</p>
     </div>
   );
 };
