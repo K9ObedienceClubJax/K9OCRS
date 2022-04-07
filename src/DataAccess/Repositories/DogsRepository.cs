2c8e209a38bf0cc21163ec1988b77522df8de16c
Fernando <jimenezf629@gmail.com>
2022-04-06 21:15:20 -0400

    SBIGNIT-108 - Implemented UpdateImage() method in DogsRepository.cs

--- src/DataAccess/Repositories/DogsRepository.cs
+++ src/DataAccess/Repositories/DogsRepository.cs
@@ -6,7 +6,6 @@ using System.Data;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
-
 namespace DataAccess.Repositories
 {
     public class DogsRepository : BaseRepository<Dog>, IDogRepository
@@ -15,5 +14,11 @@ namespace DataAccess.Repositories
         // Everytime that you create a repository, make sure you include a constructor that calls the "base constructor"
         // passing in the Db table name that is associated to it by using this syntax
         public DogsRepository(IHttpContextAccessor _httpContextAccessor) : base(DbTables.Get(nameof(Dog)), _httpContextAccessor) { }
+
+        public async Task<int> UpdateImage(IDbConnection conn, int dogId, string filename)
+        {
+            var query = $"UPDATE {_tableName} SET ImageFilename=@Filename WHERE ID=@ID";
+            return await conn.ExecuteAsync(query, new { ID = dogId, Filename = filename });
+        }
     }
 }
