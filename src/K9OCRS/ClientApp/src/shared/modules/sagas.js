e6c23974c935ab0be4f17d626ef89475634b546d
David Rojas <deznorth@gmail.com>
2022-04-20 16:02:43 -0400

    Fix merge conflict dum dum issue

--- src/K9OCRS/ClientApp/src/shared/modules/sagas.js
+++ src/K9OCRS/ClientApp/src/shared/modules/sagas.js
@@ -6,19 +6,19 @@ import * as accountsApi from '../../util/apiClients/userAccounts';
 const log = debug('saga:shared');
 
 function* example() {
-  // This is an example saga, we can use sagas to make api calls and store their results on our redux store
-  // in this case, I'm just setting a simple value on the redux store.
-  log('Running example saga');
-  yield put(actions.setExampleSagaResult('Hello from the example saga!'));
+    // This is an example saga, we can use sagas to make api calls and store their results on our redux store
+    // in this case, I'm just setting a simple value on the redux store.
+    log('Running example saga');
+    yield put(actions.setExampleSagaResult('Hello from the example saga!'));
 }
 
 function* refreshLogin() {
-  // Use try/catch blocks whenever making API calls for error handling
-  try {
-    log('Logging in');
-    // Below, call the api and get the data, transform the data however it needs to be transformed, if it needs to be and then update the redux store
-    //Hit endpoint
-    const response = yield call(accountsApi.loginStatus);
+    // Use try/catch blocks whenever making API calls for error handling
+    try {
+        log('Logging in');
+        // Below, call the api and get the data, transform the data however it needs to be transformed, if it needs to be and then update the redux store
+        //Hit endpoint
+        const response = yield call(accountsApi.loginStatus);
 
         // Then put an action so the reducer is updated with the new data
         if (!response?.data) {
@@ -32,14 +32,11 @@ function* refreshLogin() {
         log('An error ocurred on login', err);
         yield put(actions.loginRefreshed(null));
     }
-  } catch (err) {
-    log('An error ocurred on login', err);
-  }
 }
 
 const sagas = [
-  takeEvery(actions.exampleSaga, example),
-  takeEvery(actions.refreshLogin, refreshLogin),
+    takeEvery(actions.exampleSaga, example),
+    takeEvery(actions.refreshLogin, refreshLogin),
 ];
 
 export default sagas;
