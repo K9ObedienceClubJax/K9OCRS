fe796baf8f55840218e92666f48d576b39aa3f65
David Rojas <deznorth@gmail.com>
2022-04-28 09:31:15 -0400

    Fix linting warnings 2

--- src/K9OCRS/ClientApp/src/areas/applications/AppManagement/components/AppDetails.js
+++ src/K9OCRS/ClientApp/src/areas/applications/AppManagement/components/AppDetails.js
@@ -1,47 +1,43 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
-import { Button, Spinner } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import moment from 'moment-timezone';
+import { useParams } from 'react-router-dom';
+import { Button } from 'reactstrap';
 import PageHeader from 'src/shared/components/PageHeader';
 import selectors from '../../../../shared/modules/selectors';
 import PageBody from 'src/shared/components/PageBody';
 
 const AppDetails = (props) => {
-
     const { id } = useParams();
 
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true); // eslint-disable-line
     const [alerts, setAlerts] = useState([]);
 
     const [appData, setAppData] = useState([]);
 
     useEffect(() => {
         async function getTest() {
-                try {
-                    const res = await axios.get(`/api/Applications/${id}`);
-                    setAppData(res?.data);
-                    setLoading(false);
-                } catch (err) {
-                    setLoading(false);
-                    setAlerts([
-                        {
-                            color: 'danger',
-                            message: "We're having issues getting your Application information",
-                        },
-                    ]);
-                }
+            try {
+                const res = await axios.get(`/api/Applications/${id}`);
+                setAppData(res?.data);
+                setLoading(false);
+            } catch (err) {
+                setLoading(false);
+                setAlerts([
+                    {
+                        color: 'danger',
+                        message: "We're having issues getting your Application information",
+                    },
+                ]);
+            }
         }
         getTest();
     }, [id]);
 
-
     return (
         <div>
-           <PageHeader
-                title="Manage Applications" 
+            <PageHeader
+                title="Manage Applications"
                 breadCrumbItems={[
                     { label: 'Management', path: '/Manage' },
                     { label: 'Applications', path: '/Manage/Applications' },
@@ -50,16 +46,19 @@ const AppDetails = (props) => {
                 alerts={alerts}
                 setAlerts={setAlerts}
             >
-                <Button color="secondary" outline>Cancel</Button>
+                <Button color="secondary" outline>
+                    Cancel
+                </Button>
                 <Button color="primary">Save Changes</Button>
             </PageHeader>
             <PageBody>
-                <p>{id} | {appData.id} | {appData.mainAttendee}</p>
+                <p>
+                    {id} | {appData.id} | {appData.mainAttendee}
+                </p>
                 <div>Application Details</div>
                 <div>Class Details</div>
                 <div>Dog Details</div>
             </PageBody>
-            
         </div>
     );
 };
