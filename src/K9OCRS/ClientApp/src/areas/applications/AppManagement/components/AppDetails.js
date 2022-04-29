50a4df411179cfd1f27b78ff0d84b27b854b8880
Jonathan <jonathan.d.depaul@gmail.com>
2022-04-29 13:05:09 -0400

    Fixed AppDetail page
    Fixing bad merge

--- src/K9OCRS/ClientApp/src/areas/applications/AppManagement/components/AppDetails.js
+++ src/K9OCRS/ClientApp/src/areas/applications/AppManagement/components/AppDetails.js
@@ -11,10 +11,12 @@ import SectionData from './SectionData';
 import DogData from './DogData';
 
 const AppDetails = (props) => {
+
+
     const { id } = useParams();
     const navigate=useNavigate();
 
-    const [loading, setLoading] = useState(true); // eslint-disable-line
+    const [loading, setLoading] = useState(true);
     const [alerts, setAlerts] = useState([]);
 
     const [appData, setAppData] = useState([]);
@@ -45,28 +47,57 @@ const AppDetails = (props) => {
 
     useEffect(() => {
         async function getTest() {
-            try {
-                const res = await axios.get(`/api/Applications/${id}`);
-                setAppData(res?.data);
-                setLoading(false);
-            } catch (err) {
-                setLoading(false);
-                setAlerts([
-                    {
-                        color: 'danger',
-                        message: "We're having issues getting your Application information",
-                    },
-                ]);
-            }
+                try {
+                    const res = await axios.get(`/api/Applications/${id}`);
+                    setAppData(res?.data);
+                    setLoading(false);
+                } catch (err) {
+                    setLoading(false);
+                    setAlerts([
+                        {
+                            color: 'danger',
+                            message: "We're having issues getting your Application information",
+                        },
+                    ]);
+                }
         }
         getTest();
     }, [id]);
 
+    async function saveData(event) {
+       // event.preventDefault();
+
+        const id = appData?.id;
+        if (id) {
+            try {
+              const response = await axios.put(`/api/applications/ ${id}`, appUpdateData, {
+                headers: {
+                  "x-access-token": "token-value",
+                },
+              });
+              navigate('/Manage/Applications');
+              return response.data;
+              
+            } catch (err) {
+              console.error(err);
+            }
+           }
+        };
+
+        const onSubmit = async (event) => {
+            event.preventDefault();
+            try {
+                await saveData();
+                alert('Your changes were saved');
+            }
+            catch (e) {alert('Something went wrong! Changes were not saved')};
+        }
+
 
     return (
         <div>
-            <PageHeader
-                title="Manage Applications"
+           <PageHeader
+                title="Manage Applications" 
                 breadCrumbItems={[
                     { label: 'Management', path: '/Manage' },
                     { label: 'Applications', path: '/Manage/Applications' },
@@ -75,15 +106,90 @@ const AppDetails = (props) => {
                 alerts={alerts}
                 setAlerts={setAlerts}
             >
-                <Button color="secondary" outline>Cancel</Button>
-                <Button color="primary">Save Changes</Button>
+                <Button color="secondary" outline >Cancel</Button>
+                <Button color="primary" onClick={onSubmit}>Save Changes</Button>
             </PageHeader>
             <PageBody>
-                <p>{id} | {appData.id} | {appData.mainAttendee}</p>
-                <div>Application Details</div>
-                <div>Class Details</div>
-                <div>Dog Details</div>
+                <Row className='pb-3'>
+                    <Col className='cardsurface mx-3'>
+                            <h4>Application Details</h4>
+                            <div className='ps-3'>
+                                <p className='my-1'><b>Applicant:</b>&nbsp;&nbsp;{appData?.modifiedByName}</p>
+                                <InputGroup >
+                                    <Label for='payMethod' className='my-1'><b>Application Status:</b>&nbsp;&nbsp;</Label>
+                                    <div className='w-75'><Input
+                                        bsSize="sm"
+                                        className="mb-2 w-auto"
+                                        id='status'
+                                        name='status'
+                                        type='select'
+                                        onChange={handleSelectPayment}
+                                    >
+                                        {appData?.status === 'Pending' ? <option value='Pending' selected>Pending</option> : <option value='Pending'>Pending</option>}
+                                        {appData?.status === 'Active' ? <option value='Active' selected>Active</option> : <option value='Active'>Active</option>}
+                                        {appData?.status === 'Completed' ? <option value='Completed' selected>Completed</option> : <option value='Completed'>Completed</option>}
+                                        {appData?.status === 'Cancelled' ? <option value='Cancelled' selected>Cancelled</option> : <option value='Cancelled'>Cancelled</option>}
+                                    </Input></div>
+                                </InputGroup> 
+{/*                                 <InputGroup check inline>
+                                <p className='my-1'><b>Payment Status</b>&nbsp;&nbsp;
+                                <Input type='checkbox'
+                                    checked={isPaidStatus}
+                                    onChange={handleCheckPaid}
+                                    disabled={loading} />
+                                <Label check>&nbsp;&nbsp;Paid&nbsp;&nbsp;&nbsp;&nbsp;</Label>
+                                <Input type='checkbox'
+                                    checked={isRefunded}
+                                    onChange={(e) => setIsRefunded(e.target.checked)}
+                                    disabled={loading} />
+                                <Label check>&nbsp;&nbsp;Refunded</Label>
+                                </p ></InputGroup> */}
+
+                                <InputGroup>
+                                <Label for='payment'><b>Payment Status: </b></Label>
+                                <div>This applicant has paid: &nbsp;&nbsp;
+                                    <Label check>
+                                        <Input type='radio' name='radio1' onClick={() => setIsPaidStatus(true)} />{''} Yes&nbsp;&nbsp;
+                                    </Label>
+
+                                    <Label check>
+                                        <Input type='radio' name='radio1' onClick={() => setIsPaidStatus(false)} />{''} No
+                                    </Label>
+                                </div>
+                                </InputGroup>
+                                <p>Paid status: {isPaidStatus ? 'Yes' : 'No'}</p>
+
+                                <InputGroup>
+                                <Label for='payment'><b>Refund Status: </b></Label>
+                                <div>This applicant has been refunded: &nbsp;&nbsp;
+                                    <Label check>
+                                        <Input type='radio' name='radio2' onClick={() => setIsRefunded(true)} /> Yes&nbsp;&nbsp;
+                                    </Label>
+
+                                    <Label check>
+                                        <Input type='radio' name='radio2' onClick={() => setIsRefunded(false)} /> No
+                                    </Label>
+                                </div>
+                                </InputGroup>
+                                <p>Paid status: {isRefunded ? 'Yes' : 'No'}</p>
+
+                                <p className='my-1'><b>Main Attendee:</b>&nbsp;&nbsp;{appData?.mainAttendee}</p>
+                                <p className='my-1'><b>Additional Attendees:</b>&nbsp;&nbsp;{appData?.additionalAttendees}</p>
+                            </div>
+                    </Col>
+                    <Col className='cardsurface mx-3'>
+                        <SectionData id={appData?.classSectionID} />
+                    </Col>
+
+                </Row>
+                <Row className='pt-3'>
+                <Col className='cardsurface mx-3'>
+                    <DogData id={appData?.dogID} />
+                    </Col>
+
+                </Row>
             </PageBody>
+            
         </div>
     );
 };
