b40852803f311c9483827d0c232f6682e00f9b1d
David Rojas <deznorth@gmail.com>
2022-05-05 16:24:41 -0400

    SBIGNIT-142 - Improvements to responsive tables and bug fix

--- src/K9OCRS/ClientApp/src/areas/management/Users/columns.js
+++ src/K9OCRS/ClientApp/src/areas/management/Users/columns.js
@@ -7,66 +7,62 @@ import { BsCheckLg } from 'react-icons/bs';
 const USER_ROLES_KEYS = Object.keys(USER_ROLES);
 
 const idTemplate = ({ value }) => {
-  return value;
+    return value;
 };
 
 const userTemplate = ({ value }) => {
-  return (
-    <div>
-      <ProfileBadge
-        {...value}
-        imageUrl={value.profilePictureUrl}
-        link
-      />
-    </div>
-  );
+    return (
+        <div>
+            <ProfileBadge {...value} imageUrl={value.profilePictureUrl} link />
+        </div>
+    );
 };
 
 const roleTemplate = ({ value }) => {
-  return USER_ROLES_KEYS.find(key => USER_ROLES[key] === value);
+    return USER_ROLES_KEYS.find((key) => USER_ROLES[key] === value);
 };
 
 const emailTemplate = ({ value }) => {
-  return value;
+    return value;
 };
 
-const membershipTemplate = ({ value }) => value ? <BsCheckLg className="text-success" /> : '';
+const membershipTemplate = ({ value }) => (value ? <BsCheckLg className="text-success" /> : '--');
 
 // const dogTemplate = ({ value }) => {
 //   return value;
 // };
 
 const columns = [
-  {
-    Header: 'ID',
-    accessor: 'id',
-    Cell: idTemplate,
-  },
-  {
-    Header: 'Name',
-    accessor: (row) => ({
-      id: row.id,
-      firstName: row.firstName,
-      lastName: row.lastName,
-      profilePictureUrl: row.profilePictureUrl,
-    }),
-    Cell: alignmentWrapper('left', userTemplate),
-  },
-  {
-    Header: 'Email',
-    accessor: 'email',
-    Cell: emailTemplate,
-  },
-  {
-    Header: 'Membership',
-    accessor: 'isMember',
-    Cell: membershipTemplate,
-  },
-  {
-    Header: 'Role',
-    accessor: 'userRoleID',
-    Cell: roleTemplate,
-  },
+    {
+        Header: 'ID',
+        accessor: 'id',
+        Cell: idTemplate,
+    },
+    {
+        Header: 'Name',
+        accessor: (row) => ({
+            id: row.id,
+            firstName: row.firstName,
+            lastName: row.lastName,
+            profilePictureUrl: row.profilePictureUrl,
+        }),
+        Cell: alignmentWrapper('left', userTemplate),
+    },
+    {
+        Header: 'Email',
+        accessor: 'email',
+        Cell: emailTemplate,
+    },
+    {
+        Header: 'Membership',
+        accessor: 'isMember',
+        Cell: membershipTemplate,
+    },
+    {
+        Header: 'Role',
+        accessor: 'userRoleID',
+        Cell: roleTemplate,
+    },
 ];
 
 export default columns;
