51f6607e06a297a2c638cc2b17ef3fc2f91b43cf
Fernando <jimenezf629@gmail.com>
2022-05-11 02:31:36 -0400

    SBIGNIT-154 - Added DogManagement route to App.js

---
+++ src/K9OCRS/ClientApp/src/areas/dogs/DogManagement/index.js
@@ -0,0 +1,18 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import PageHeader from '../../../shared/components/PageHeader';
+import Table from '../../../shared/components/Table';
+import PageBody from '../../../shared/components/PageBody';
+
+const DogManagement = (props) => {
+    const [loading, setLoading] = useState(true);
+
+
+
+    return (
+        <div>
+            hello world
+        </div>
+    );
+};
+
+export default DogManagement;
