d8c9021068f304488d6f2ad747a245f577d67e17
David Rojas <deznorth@gmail.com>
2022-05-13 17:05:06 -0400

    SBIGNIT-158 - Remove SideNav component

--- src/K9OCRS/ClientApp/src/areas/accounts/index.js
+++ src/K9OCRS/ClientApp/src/areas/accounts/index.js
@@ -1,34 +1,23 @@
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import selectors from '../../shared/modules/selectors';
-import { Row, Col, Button } from 'reactstrap';
+import { Row, Button } from 'reactstrap';
 import PageHeader from '../../shared/components/PageHeader';
-import SideNav from '../../shared/components/SideNav';
 import Profile from '../../shared/components/Profile';
 
 const MyAccount = (props) => {
-  const { currentUser = null } = props;
-  const [alerts, setAlerts] = useState([]);
+    const [alerts, setAlerts] = useState([]);
 
-  return (
-    <div>
-      <PageHeader title='My Account' alerts={alerts} setAlerts={setAlerts}>
-        <Button color='primary' form='profileForm'>
-          Save Changes
-        </Button>
-      </PageHeader>
-      <Row>
-        <Col lg='2'>
-          <SideNav userRoleID={currentUser.userRoleID} />
-        </Col>
-        <Col lg={{ size: 8, offset: 1 }}>
-          <Profile mode='default' setAlerts={setAlerts} />
-        </Col>
-      </Row>
-    </div>
-  );
+    return (
+        <div>
+            <PageHeader title="My Account" alerts={alerts} setAlerts={setAlerts}>
+                <Button color="primary" form="profileForm">
+                    Save Changes
+                </Button>
+            </PageHeader>
+            <Row>
+                <Profile mode="default" setAlerts={setAlerts} />
+            </Row>
+        </div>
+    );
 };
 
-export default connect((state) => ({
-  currentUser: selectors.selectCurrentUser(state),
-}))(MyAccount);
+export default MyAccount;
