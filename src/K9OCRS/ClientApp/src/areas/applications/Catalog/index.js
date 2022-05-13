a60fdff812d9faacc8451fb5403f2ab8483def66
David Rojas <deznorth@gmail.com>
2022-05-13 17:24:47 -0400

    SBIGNIT-156 - Added pagination to the class catalog

--- src/K9OCRS/ClientApp/src/areas/applications/Catalog/index.js
+++ src/K9OCRS/ClientApp/src/areas/applications/Catalog/index.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Container, Row, Spinner } from 'reactstrap';
+import { useSearchParams } from 'react-router-dom';
 import * as classTypesClient from '../../../util/apiClients/classTypes';
 import PageHeader from '../../../shared/components/PageHeader';
 import ClassCard from './components/ClassCard';
@@ -13,7 +14,23 @@ const Catalog = () => {
     const [loading, setLoading] = useState(true);
     const [alerts, setAlerts] = useState([]);
     const [classTypes, setClassTypes] = useState([]);
-    const [page, setPage] = useState(0);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [page, setPage] = useState((c) => {
+        if (searchParams.has('page')) {
+            return searchParams.get('page') - 1;
+        }
+        return 0;
+    });
+
+    const isMounted = useRef(false);
+
+    // This actually just updates the page param
+    // the actual page change is handled by useEffect
+    const handlePageChange = (targetPage) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', targetPage + 1);
+        setSearchParams(params);
+    };
 
     // Calculate the start and end of our paginated subset of class types
     const pageStartIndex = page * itemsPerPage;
@@ -49,6 +66,27 @@ const Catalog = () => {
         getTest();
     }, []);
 
+    // reset the page if the filteredClasses count changes
+    useEffect(() => {
+        if (!loading && isMounted.current) {
+            const params = new URLSearchParams(searchParams);
+            params.delete('page');
+            setSearchParams(params);
+        } else {
+            isMounted.current = true;
+        }
+    }, [filteredClasses.length]); // eslint-disable-line
+
+    // actually change page when the param changes
+    const searchParamsString = searchParams.toString();
+    useEffect(() => {
+        if (searchParams.has('page')) {
+            setPage(searchParams.get('page') - 1);
+        } else {
+            setPage(0);
+        }
+    }, [searchParamsString]); // eslint-disable-line
+
     return (
         <div>
             <PageHeader title="Class Catalog" alerts={alerts} setAlerts={setAlerts} />
@@ -73,7 +111,7 @@ const Catalog = () => {
                 </Container>
                 <div className="d-flex justify-content-end px-4 px-lg-5">
                     <CustomPagination
-                        onPageChange={setPage}
+                        onPageChange={handlePageChange}
                         totalCount={classTypes?.length}
                         currentPage={page}
                         pageSize={itemsPerPage}
